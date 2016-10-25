35307028a0e9f51e90b9a67bc85064b24e4ef357
indolering <zachlym@indolering.com>
2016-10-25 11:50:50 -0700

    I'll annotate my commits after the audit is over

--- blang.js
+++ blang.js
@@ -2,6 +2,6 @@ window.setInterval(function () {
   var blinks = document.getElementsByTagName("blink");
   var b = blinks.length;
   while(b--){
-    blinks[b].style.visibility = (blinks[b].style.visibility === 'visible') ? 'hidden' : 'visible';
+    blinks[b].style.visibility = (blinks[b].style.visibility == 'visible') ? 'hidden' : 'visible';
   }
 }, 250);

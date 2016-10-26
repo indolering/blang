440cc975ba40e108f1e814d11d9d749aca4641ad
indolering <zachlym@indolering.com>
2016-10-25 19:34:29 -0700

    I never touched those files, look at them!  I don't think so.   Sad.

--- blang.js
+++ blang.js
@@ -1,9 +1,14 @@
 window.setInterval(function () {
-  var blinks = document.getElementsByTagName("blink");
-  var b = blinks.length;
-  while(b--){
-    blinks[b].style.visibility = (blinks[b].style.visibility == 'visible') ? 'hidden' : 'visible';
+  blink(document.getElementsByTagName("blink"));
+  blink(document.getElementsByClassName("blang"));
+
+  function blink(elements) {
+    var b = elements.length;
+    while(b--){
+      elements[b].style.visibility = (elements[b].style.visibility == 'visible') ? 'hidden' : 'visible';
+    }
   }
+
 }, 250);
 
 String.prototype.blink = function () {
